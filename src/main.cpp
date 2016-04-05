6a886c2383c248b026e972eacb74e34c045d5665
clstl <reamles@gmail.com>
2016-04-06 00:12:15 +0300

    Added thread support

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 int nx = 800;
 int ny = 600;
-int ns = 100;
+int ns = 10;
 
 math::vec3f random_in_unit_sphere() {
     math::vec3f p;
@@ -21,6 +21,7 @@ math::vec3f random_in_unit_sphere() {
     return p;
 }
 
+std::mutex mutex2;
 math::vec3f color(const math::Ray &r, geometry::Hitable *world) {
     geometry::HitRecord rec;
     if (world->hit(r, 0, MAXFLOAT, rec)) {
@@ -31,6 +32,7 @@ math::vec3f color(const math::Ray &r, geometry::Hitable *world) {
         float t = 0.5f * (unit_direction.y + 1.0f);
         return math::vec3f{1.0f, 1.0f, 1.0f} * (1.0f - t) + math::vec3f{0.5f, 0.7f, 1.0f} * t;
     }
+
 }
 
 
@@ -60,19 +62,17 @@ void func(std::shared_ptr<geometry::HitableList> &list, sf::Image &buffer,
 
             c = c / float(ns);
             c = math::vec3f(sqrt(c.x), sqrt(c.y), sqrt(c.z));
-//            mutex.lock();
             colors[j * rect.width + i] = c;
-//            mutex.unlock();
         }
 
     }
-    mutex.lock();
     for (int j = rect.top; j < rect.height; ++j) {
         for (int i = rect.left; i < rect.width; ++i) {
+            mutex.lock();
             buffer.setPixel(i, j, toColor(colors[j * rect.width + i]));
+            mutex.unlock();
         }
     }
-    mutex.unlock();
 }
 
 
@@ -86,9 +86,11 @@ int main(int argc, char **argv) {
     buffer.create(nx, ny, sf::Color::Black);
     Camera cam{ratio};
 
-    geometry::Hitable *list[2];
+    geometry::Hitable *list[3];
     list[0] = new geometry::Sphere({0, 0, -1}, 0.5f);
     list[1] = new geometry::Sphere({0, -100.5f, -1}, 100.0f);
+//    list[2] = new geometry::Sphere({2, 2, -10}, 0.5f);
+//    list[3] = new geometry::Sphere({1, 2, -100}, 1.5f);
 
 
     std::shared_ptr<geometry::HitableList> toWorld
@@ -119,18 +121,18 @@ int main(int argc, char **argv) {
 //    thread1.join();
 
 
-    std::thread a(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{0, 0, nx / 2, ny / 2}, ns);
-    std::thread c(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{nx / 2, 0, nx, ny / 2}, ns);
-    std::thread b(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{nx / 2, ny / 2, nx, ny}, ns);
-    std::thread d(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{0, ny / 2, nx / 2, ny}, ns);
-    b.join();
-    a.join();
-    c.join();
-    d.join();
+    func((toWorld), (buffer), (cam), sf::IntRect{0, 0, nx / 1, ny / 1}, ns);
+//    std::thread a(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{0, 0, nx / 2, ny / 2}, ns);
+//    std::thread c(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{nx / 2, 0, nx, ny / 2}, ns);
+//    std::thread b(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{nx / 2, ny / 2, nx, ny}, ns);
+//    std::thread d(func, std::ref(toWorld), std::ref(buffer), std::ref(cam), sf::IntRect{0, ny / 2, nx / 2, ny}, ns);
+//    a.join();
+//    b.join();
+//    c.join();
+//    d.join();
 
     sf::Texture buffer_texture;
-    buffer_texture.
-            loadFromImage(buffer);
+    buffer_texture.loadFromImage(buffer);
     sf::Sprite sprite(buffer_texture);
 
     while (window.
